4a77f13a73b5869373570384d4bbdcb59b37d783
Patrick Wang <kk1fff@patrickz.net>
2013-05-29 18:11:05 +0800

    Bug 825708 - Part 1: use nr_local_addr in nr_stun_get_addrs and provide interface type/estimated speed/vpn information for Linux and B2G.

--- media/mtransport/gonk_addrs.cpp
+++ media/mtransport/gonk_addrs.cpp
@@ -23,6 +23,7 @@ namespace {
 struct NetworkInterface {
   struct sockaddr_in addr;
   std::string name;
+  int type;
 };
 
 nsresult
@@ -74,6 +75,19 @@ GetInterfaces(std::vector<NetworkInterface>* aInterfaces)
     }
     interface.name = NS_ConvertUTF16toUTF8(ifaceName).get();
 
+    int32_t type;
+    if (NS_FAILED(iface->GetType(&type))) {
+      continue;
+    }
+    switch (type) {
+    case nsINetworkInterface::NETWORK_TYPE_WIFI:
+      interface.type = NR_INTERFACE_TYPE_WIFI;
+      break;
+    case nsINetworkInterface::NETWORK_TYPE_MOBILE:
+      interface.type = NR_INTERFACE_TYPE_MOBILE;
+      break;
+    }
+
     aInterfaces->push_back(interface);
   }
   return NS_OK;
@@ -81,7 +95,7 @@ GetInterfaces(std::vector<NetworkInterface>* aInterfaces)
 } // anonymous namespace
 
 int
-nr_stun_get_addrs(nr_transport_addr aAddrs[], int aMaxAddrs,
+nr_stun_get_addrs(nr_local_addr aAddrs[], int aMaxAddrs,
                   int aDropLoopback, int* aCount)
 {
   nsresult rv;
@@ -106,11 +120,14 @@ nr_stun_get_addrs(nr_transport_addr aAddrs[], int aMaxAddrs,
     NetworkInterface &interface = interfaces[i];
     if (nr_sockaddr_to_transport_addr((sockaddr*)&(interface.addr),
                                       sizeof(struct sockaddr_in),
-                                      IPPROTO_UDP, 0, &(aAddrs[n]))) {
+                                      IPPROTO_UDP, 0, &(aAddrs[n].addr))) {
       r_log(NR_LOG_STUN, LOG_WARNING, "Problem transforming address");
       return R_FAILED;
     }
-    strlcpy(aAddrs[n].ifname, interface.name.c_str(), sizeof(aAddrs[n].ifname));
+    strlcpy(aAddrs[n].addr.ifname, interface.name.c_str(),
+            sizeof(aAddrs[n].addr.ifname));
+    aAddrs[n].interface.type = interface.type;
+    aAddrs[n].interface.estimated_speed = 0;
     n++;
   }
 
@@ -122,7 +139,7 @@ nr_stun_get_addrs(nr_transport_addr aAddrs[], int aMaxAddrs,
 
   for (int i = 0; i < *aCount; ++i) {
     r_log(NR_LOG_STUN, LOG_DEBUG, "Address %d: %s on %s", i,
-          aAddrs[i].as_string, aAddrs[i].ifname);
+          aAddrs[i].addr.as_string, aAddrs[i].addr.ifname);
   }
 
   return 0;
