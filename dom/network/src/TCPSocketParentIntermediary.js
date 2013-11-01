0bb6388f8df2d8477990a66e8bd53ea2b37f3552
Patrick Wang <kk1fff@patrickz.net>
2013-11-01 17:10:03 +0800

    Bug 932183: Update bufferedAmount in child more frequently

--- dom/network/src/TCPSocketParentIntermediary.js
+++ dom/network/src/TCPSocketParentIntermediary.js
@@ -28,7 +28,11 @@ TCPSocketParentIntermediary.prototype = {
         };
       }
     );
- },
+  },
+
+  _onUpdateBufferedAmountHandler: function(aParentSide, aBufferedAmount, aTrackingNumber) {
+    aParentSide.sendUpdateBufferedAmount(aBufferedAmount, aTrackingNumber);
+  },
 
   open: function(aParentSide, aHost, aPort, aUseSSL, aBinaryType, aAppId) {
     let baseSocket = Cc["@mozilla.org/tcp-socket;1"].createInstance(Ci.nsIDOMTCPSocket);
@@ -40,6 +44,10 @@ TCPSocketParentIntermediary.prototype = {
     if (socketInternal) {
       socketInternal.setAppId(aAppId);
     }
+
+    // Handle parent's request to update buffered amount.
+    socketInternal.setOnUpdateBufferedAmountHandler(
+      this._onUpdateBufferedAmountHandler.bind(this, aParentSide));
 
     // Handlers are set to the JS-implemented socket object on the parent side.
     this._setCallbacks(aParentSide, socket);
@@ -79,12 +87,16 @@ TCPSocketParentIntermediary.prototype = {
     return serverSocket;
   },
 
-  sendString: function(aData) {
-    return this._socket.send(aData);
+  sendString: function(aData, aTrackingNumber) {
+    let socketInternal = this._socket.QueryInterface(Ci.nsITCPSocketInternal);
+    return socketInternal.sendFromChild(aData, null, null,
+                                        aTrackingNumber);
   },
 
-  sendArrayBuffer: function(aData) {
-    return this._socket.send(aData, 0, aData.byteLength);
+  sendArrayBuffer: function(aData, aTrackingNumber) {
+    let socketInternal = this._socket.QueryInterface(Ci.nsITCPSocketInternal);
+    return socketInternal.sendFromChild(aData, 0, aData.byteLength,
+                                      aTrackingNumber);
   },
 
   classID: Components.ID("{afa42841-a6cb-4a91-912f-93099f6a3d18}"),
